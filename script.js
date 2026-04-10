f43545580b0720b98d1b34a2618737b6b6e274fa
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2026-04-10 08:57:45 -0300

    js

--- script.js
+++ script.js
@@ -7,6 +7,28 @@ const newsArticle= document.querySelector('.news')
 
 const loadPreferences = () => {
     // cor
+
+    const saveColor = localStorage.getItem('primaryColor');
+    if (saveColor) {
+        document.documentElement.style.setProperty('--primary-color', saveColor)
+        colorPicker.value = saveColor;
+    }
+
+    const saveArticleColor = localStorage.getItem('articleColor');
+    if (saveArticleColor) {
+
+    }
+
+    const savedFont = localStorage.getItem('fontFamily');
+    if (savedFont) {
+        
+    }
+
+    const saveTheme = localStorage.getItem('theme');
+    if (saveTheme === 'dark') {
+        body.classList.add('dark');
+        themeToggle.innerHTML = '<span class="material-icons">light_mode</span>'
+    }
 };
 
 colorPicker.addEventListener('input', (e) => {
@@ -27,9 +49,18 @@ articleColorPicker.addEventListener('input', (e) => {
 });
 
 fontSelect.addEventListener('change', (e) => {
-
+     const font= e.target.value;
+     document.documentElement.style.setProperty('--font-family', font)
+    localStorage.setItem('fontFamily', font)
 });
 
 themeToggle.addEventListener('click', (e) => {
-
+    body.classList.toggle('dark');
+    const isDark = body.classList.contains('dark');
+    themeToggle.innerHTML = isDark
+    ? '<span class="material-icons">light_mode</span>'
+    : '<span class="material-icons">dark_mode</span>';
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
 });
+
+loadPreferences();
